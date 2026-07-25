958d120bd85a67eb42673fad1cde68580fe586b4
thngoc0000 <ngoctr2k4@gmail.com>
2026-07-25 14:50:36 +0700

    Luyện Collections

---
+++ Practice/Collections/Collections/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Practice/Collections/Collections/UseDictionary.cs
@@ -0,0 +1,67 @@
+﻿using Collections;
+
+var user = new Dictionary<string, Product>();
+
+bool isRunning = true;
+while (isRunning)
+{
+    Console.WriteLine("1. Add new product");
+    Console.WriteLine("2. Delete product");
+    Console.WriteLine("3. Find product");
+    Console.WriteLine("4. Show all products");
+    Console.WriteLine("5. Exit");
+    Console.Write("Select your options:");
+
+    int input = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("===================================");
+
+    if (input == 5)
+    {
+        isRunning = false;
+        break;
+    }
+    else if (input == 1)
+    {
+        var product = new Product();
+        Console.Write("Enter product name:");
+        product.Name = Console.ReadLine() ?? string.Empty;
+        Console.Write("Enter product description:");
+        product.Description = Console.ReadLine() ?? string.Empty;
+        user.Add(product.Name, product);
+    }
+    else if (input == 2)
+    {
+        Console.Write("Enter product name need to delete:");
+        user.Remove(Console.ReadLine() ?? string.Empty);
+    }
+    else if (input == 3)
+    {
+        Console.Write("Enter product name need to find:");
+        string productName = Console.ReadLine() ?? string.Empty;
+        if (user.TryGetValue(productName, out var product))
+        {
+            Console.WriteLine($"Product found: {product.Name} - {product.Description}");
+        }
+        else
+        {
+            Console.WriteLine("Product not found.");
+        }
+    }
+    else if (input == 4)
+    {
+        if (user == null || user.Count == 0)
+        {
+            Console.WriteLine("No products available.");
+        }
+        else
+        {
+            foreach (var kvp in user)
+            {
+                Console.WriteLine($"Product: {kvp.Value.Name} - {kvp.Value.Description}");
+            }
+        }
+    }
+
+    Console.WriteLine("===================================");
+}
